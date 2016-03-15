1a09c356e3f90e6e7c91526233200facbebe1453
kayhide <kayhide@gmail.com>
2016-03-15 22:06:49 +0900

    update seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-Segment.find_or_create_by(name: 'paid', label: 'トップ有料 App')
-Segment.find_or_create_by(name: 'free', label: 'トップ無料 App')
-Segment.find_or_create_by(name: 'gross', label: 'トップセールス App')
+Segment.find_or_create_by(name: 'paid', label: 'トップ有料')
+Segment.find_or_create_by(name: 'free', label: 'トップ無料')
+Segment.find_or_create_by(name: 'gross', label: 'トップセールス')
 
 Genre.find_or_create_by(name: 'all', label: '全て')
 Genre.find_or_create_by(name: 'games', label: 'ゲーム')
