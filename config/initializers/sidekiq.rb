4fee1fafd5c9912dc987f55222096b4626c560b3
kayhide <kayhide@gmail.com>
2016-04-17 01:58:47 +0900

    update production settings.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,23 +1,18 @@
 url = ENV['REDIS_URL'].presence || 'redis://localhost:6379'
+app_name = Rails.application.class.parent.name.underscore
 
 Sidekiq.configure_server do |config|
-  hash = {
+  config.redis = {
     url: url,
-    namespace: "sidekiq_bondooman_#{Rails.env}"
+    namespace: "sidekiq_#{app_name}_#{Rails.env}",
+    size: ENV['REDIS_SERVER_SIZE'].try(&:to_i) || 20
   }
-  if size = ENV['REDIS_SERVER_SIZE']
-    hash[:size] = size.to_i
-  end
-  config.redis = hash
 end
 
 Sidekiq.configure_client do |config|
-  hash = {
+  config.redis = {
     url: url,
-    namespace: "sidekiq_bondooman_#{Rails.env}"
+    namespace: "sidekiq_#{app_name}_#{Rails.env}",
+    size: ENV['REDIS_CLIENT_SIZE'].try(&:to_i) || 1
   }
-  if size = ENV['REDIS_CLIENT_SIZE']
-    hash[:size] = size.to_i
-  end
-  config.redis = hash
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,6 @@ timeout 30
 preload_app true
 
 before_fork do |server, worker|
-
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -14,7 +13,6 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
